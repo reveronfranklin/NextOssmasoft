cbe2dbfb507c1e357e3c40be14520440c27abe09
reveronfranklin <reveronfranklin@gmail.com>
2023-03-08 13:08:08 -0400

    Change hook usepresupuesto

--- src/hooks/usePresupuesto.ts
+++ src/hooks/usePresupuesto.ts
@@ -1,15 +1,16 @@
 import useSWR, { SWRConfiguration } from "swr";
+import { IPresupuesto } from '../interfaces/Presupuesto/i-presupuesto';
 
 
 const fetcher = (...args:[key:string] ) => fetch(...args).then(res => res.json());
 
 export const usePresupuesto=(url: string,config:SWRConfiguration={})=>{
 
-  const { data, error } = useSWR(`http://localhost:46196/api${url}`, fetcher,config);
+  const { data, error } = useSWR<IPresupuesto[]>(`http://localhost:46196/api${url}`, fetcher,config);
 
 
   return {
-    presupuestos:data.data || [],
+    presupuestos:data || [],
     isLoading:!error && !data,
     isError:error
   }
