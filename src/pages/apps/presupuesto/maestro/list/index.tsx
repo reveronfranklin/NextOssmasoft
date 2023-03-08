9d17f638626dee1a6f6ede15be02c70bb3c4b424
reveronfranklin <reveronfranklin@gmail.com>
2023-03-08 08:04:45 -0400

    List Maetro presupuesto

--- src/pages/apps/presupuesto/maestro/list/index.tsx
+++ src/pages/apps/presupuesto/maestro/list/index.tsx
@@ -1,53 +1,74 @@
-import { Grid } from '@mui/material'
+import { Box, Card, CardHeader} from '@mui/material'
 import React from 'react'
-import PageHeader from 'src/@core/components/page-header';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-import TableEditable from 'src/views/table/data-grid/TableEditable';
-import TableColumns from 'src/views/table/data-grid/TableColumns';
-import TableBasicSort from 'src/views/table/data-grid/TableBasicSort';
-import TableFilter from 'src/views/table/data-grid/TableFilter';
-import TableSelection from 'src/views/table/data-grid/TableSelection';
-import TableServerSide from 'src/views/table/data-grid/TableServerSide';
 
+
+import useSWR from "swr";
+import { DataGrid } from '@mui/x-data-grid';
+
+const fetcher = (...args:[key:string] ) => fetch(...args).then(res => res.json());
+const columns = [
+  {
+
+    field: 'codigoPresupuesto'
+    , headerName: 'Codigo', width: 130
+
+  },
+  {
+
+    field: 'denominacion',
+    width: 330
+
+  },
+  {
+
+    field: 'descripcion',
+    width: 100
+  },
+  {
+
+    field: 'ano',
+    width: 100
+  },
+
+
+  {
+
+    field: 'fechaDesde',
+    headerName:'Desde',
+    width: 130
+  },
+  {
+
+    field: 'fechaHasta',
+    headerName:'Hasta',
+    width: 130
+  },
+  {
+
+    field: 'montoPresupuesto',
+    headerName:'Monto'
+
+  },
+
+
+]
 const PresupuestoList = () => {
+
+  const { data, error } = useSWR("http://localhost:46196/api/PrePresupuesto/GetAll", fetcher);
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
   return (
-    <Grid container spacing={6}>
-      <PageHeader
-        title={
-          <Typography variant='h5'>
-
-            <Link href='https://mui.com/x/react-data-grid/' target='_blank'>
-            Presupuesto
-            </Link>
-          </Typography>
-        }
-        subtitle={
-          <Typography variant='body2'>
-            Mantenimiento de presupuesto por ano
-          </Typography>
-        }
-      />
-     <Grid item xs={12}>
-        <TableEditable />
-      </Grid>
-      <Grid item xs={12}>
-        <TableColumns />
-      </Grid>
-      <Grid item xs={12}>
-        <TableBasicSort />
-      </Grid>
-      <Grid item xs={12}>
-        <TableFilter />
-      </Grid>
-      <Grid item xs={12}>
-        <TableSelection />
-      </Grid>
-      <Grid item xs={12}>
-        <TableServerSide/>
-      </Grid>
-
-    </Grid>
+    <Card>
+      <CardHeader title='Maestro de Presupuesto' />
+      <Box sx={{ height: 500 }}>
+        <DataGrid
+          getRowId={(row) => row.codigoPresupuesto}
+          columns={columns}
+          rows={data.data} />
+      </Box>
+    </Card>
   )
 }
 
