cf6ae88842e4a3b4dc00601313b3c00ca98a8288
reveronfranklin <reveronfranklin@gmail.com>
2023-05-09 19:53:04 -0400

    Change Port 5000

--- src/MyApis/ossmmasofApi.ts
+++ src/MyApis/ossmmasofApi.ts
@@ -8,7 +8,7 @@ import authConfig from 'src/configs/auth'
 
 
 export const  ossmmasofApi = axios.create({
-  baseURL:'http://localhost:46196/api'
+  baseURL:'http://localhost:5000/api'
 
 });
 
--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -1,9 +1,9 @@
 
 
 export default {
-  meEndpoint: 'http://localhost:46196/api/SisUsuarios/RefreshToken',
-  loginEndpoint: 'http://localhost:46196/api/SisUsuarios/Login',
-  refreshEndPoint:'http://localhost:46196/api/SisUsuarios/RefreshToken',
+  meEndpoint: 'http://localhost:5000/api/SisUsuarios/RefreshToken',
+  loginEndpoint: 'http://localhost:5000/api/SisUsuarios/Login',
+  refreshEndPoint:'http://localhost:5000/api/SisUsuarios/RefreshToken',
   loginEndpoint1: '/jwt/login',
   registerEndpoint: '/jwt/register',
   storageTokenKeyName: 'accessToken',
