549798755bfd0172f3ccad5570532d0a71b1b233
reveronfranklin <reveronfranklin@gmail.com>
2023-05-21 18:12:51 -0400

    Add Dasboard Presupuesto en default

--- src/pages/dashboards/presupuesto/index.tsx
+++ src/pages/dashboards/presupuesto/index.tsx
@@ -218,62 +218,12 @@ useEffect(() => {
             </Card>
         </Grid>
 
-          <Grid item xs={12} md={8} sx={{ order: 0 }}>
-            <EcommerceTotalProfit />
-          </Grid>
-          <Grid item xs={12} md={4} sx={{ order: 0 }}>
-            <Grid container spacing={6}>
-              <Grid item xs={12}>
-                <EcommerceTotalSalesDonut />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <EcommerceTotalRevenue />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <EcommerceTotalSalesRadial />
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xs={12} md={6} lg={4} sx={{ order: 0 }}>
-            <EcommerceTransactions />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4} sx={{ order: 0 }}>
-            <Grid container spacing={6}>
-              <Grid item xs={12} sm={6}>
-                <CardStatisticsVerticalComponent
-                  stats='4.1k'
-                  color='error'
-                  title='Logistics'
-                  trendNumber='+25%'
-                  subtitle='Regional Logistics'
-                  icon={<Icon icon='mdi:truck-outline' />}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <CardStatisticsVerticalComponent
-                  stats='268'
-                  color='warning'
-                  title='Reports'
-                  trend='negative'
-                  trendNumber='-8%'
-                  subtitle='System Bugs'
-                  icon={<Icon icon='mdi:check' />}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <EcommerceNewVisitors />
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xs={12} md={6} lg={4} sx={{ order: 0 }}>
-            <EcommerceWebsiteStatistics />
-          </Grid>
-          <Grid item xs={12} lg={8} sx={{ order: [1, 1, 1, 0] }}>
-            <EcommerceTable />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4} sx={{ order: 0 }}>
-            <EcommerceMeetingSchedule />
-          </Grid>
+
+
+
+
+
+
         </Grid>
       </ApexChartWrapper>
     )
