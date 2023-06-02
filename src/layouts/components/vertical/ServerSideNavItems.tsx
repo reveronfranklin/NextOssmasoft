448e684d9579cac454b2d8c035b1c8a8f7c303c7
reveronfranklin <reveronfranklin@gmail.com>
2023-06-02 08:51:45 -0400

    Clean comment ServerSideNavItems

--- src/layouts/components/vertical/ServerSideNavItems.tsx
+++ src/layouts/components/vertical/ServerSideNavItems.tsx
@@ -12,47 +12,16 @@ const ServerSideNavItems = () => {
   // ** State
   const [menuItems, setMenuItems] = useState<VerticalNavItemsType>([])
 
-
-  const fetchTableData = useCallback(
-    async () => {
-
-      //const filterHistorico:FilterHistorico={desde:new Date('2023-01-01T14:29:29.623Z'),hasta:new Date('2023-04-05T14:29:29.623Z')}
-
-      const responseAll= await ossmmasofApi.get<any>('/SisUsuarios/GetMenu');
-
-
-
-      console.log('Respuesta llamando al menu======>',responseAll.data)
-
-      setMenuItems(responseAll.data)
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    []
-  )
-
   useEffect(() => {
-    /*axios.get('/api/vertical-nav/data').then(response => {
-      const menuArray = response.data
-      console.log(role)
-      setMenuItems(menuArray)
-    })*/
-    //fetchTableData();
-
 
     let menuArray: any[]=[]
     ossmmasofApi.get<any>('/SisUsuarios/GetMenu').then(response => {
 
-
-
-
-
       response.data.forEach(function(item:any) {
         //menuArray = JSON.parse(item.menu)
         menuArray=menuArray.concat(JSON.parse(item.menu));
-        console.log(menuArray);
-    });
 
-
+      });
 
       setMenuItems(menuArray)
     })
