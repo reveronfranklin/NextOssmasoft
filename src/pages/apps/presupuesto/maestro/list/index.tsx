0dd62055c68c71a34b5ea3d291ffd351dded1234
reveronfranklin <reveronfranklin@gmail.com>
2024-04-11 20:27:10 -0400

    add report resumen presupuesto en maestro

--- src/pages/apps/presupuesto/maestro/list/index.tsx
+++ src/pages/apps/presupuesto/maestro/list/index.tsx
@@ -1,32 +1,30 @@
-import { Box, Card, CardActions, CardHeader, Grid, IconButton, Tooltip, Typography} from '@mui/material'
+import { Box, Card, CardActions, CardHeader, Grid, IconButton, Tooltip, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import { ReactDatePickerProps } from 'react-datepicker'
 
 // ** Icon Imports
 import Icon from 'src/@core/components/icon'
 
-import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
+import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid'
 import { useTheme } from '@mui/material/styles'
 
 //import { usePresupuesto } from 'src/hooks/usePresupuesto';
 
+import { useDispatch } from 'react-redux'
+import { IPresupuesto } from 'src/interfaces/Presupuesto/i-presupuesto'
+import { setOperacionCrudPresupuesto, setPresupuesto, setVerPresupuestoActive } from 'src/store/apps/presupuesto'
 
-import { useDispatch } from 'react-redux';
-import { IPresupuesto } from 'src/interfaces/Presupuesto/i-presupuesto';
-import { setOperacionCrudPresupuesto, setPresupuesto, setVerPresupuestoActive } from 'src/store/apps/presupuesto';
-
-import DatePickerWrapper from 'src/@core/styles/libs/react-datepicker';
-import Spinner from 'src/@core/components/spinner';
-import { ossmmasofApi } from 'src/MyApis/ossmmasofApi';
-import { useSelector } from 'react-redux';
-import { RootState } from 'src/store';
-import dayjs from 'dayjs';
-import DialogPrePresupuestoInfo from 'src/presupuesto/maestro/views/DialogPrePresupuestoInfo';
-import { IFechaDto } from 'src/interfaces/fecha-dto';
-import { monthByIndex } from 'src/utilities/ge-date-by-object';
-
-
-
+import DatePickerWrapper from 'src/@core/styles/libs/react-datepicker'
+import Spinner from 'src/@core/components/spinner'
+import { ossmmasofApi } from 'src/MyApis/ossmmasofApi'
+import { useSelector } from 'react-redux'
+import { RootState } from 'src/store'
+import dayjs from 'dayjs'
+import DialogPrePresupuestoInfo from 'src/presupuesto/maestro/views/DialogPrePresupuestoInfo'
+import { IFechaDto } from 'src/interfaces/fecha-dto'
+import { monthByIndex } from 'src/utilities/ge-date-by-object'
+import { setReportName, setVerReportViewActive } from 'src/store/apps/report'
+import DialogReportInfo from 'src/share/components/Reports/views/DialogReportInfo'
 
 const PresupuestoList = () => {
   const theme = useTheme()
@@ -34,187 +32,197 @@ const PresupuestoList = () => {
   const popperPlacement: ReactDatePickerProps['popperPlacement'] = direction === 'ltr' ? 'bottom-start' : 'bottom-end'
   const fechaActual = new Date()
 
-  const currentYear  = new Date().getFullYear();
-  const currentMonth = new Date().getMonth();
+  const currentYear = new Date().getFullYear()
+  const currentMonth = new Date().getMonth()
 
-  const currentMonthString ='00' + monthByIndex(currentMonth).toString();
+  const currentMonthString = '00' + monthByIndex(currentMonth).toString()
 
-  const currentDay =new Date().getDate();
-  const currentDayString = '00' + currentDay.toString();
-  const defaultDate :IFechaDto = {year:currentYear.toString(),month:currentMonthString.slice(-2),day:currentDayString.slice(-2)}
-  const defaultDateString = fechaActual.toISOString();
+  const currentDay = new Date().getDate()
+  const currentDayString = '00' + currentDay.toString()
+  const defaultDate: IFechaDto = {
+    year: currentYear.toString(),
+    month: currentMonthString.slice(-2),
+    day: currentDayString.slice(-2)
+  }
+  const defaultDateString = fechaActual.toISOString()
 
   console.log(monthByIndex(currentMonth))
+  interface CellType {
+    row: IPresupuesto
+  }
 
   const columns = [
-
-
     {
-
-      field: 'codigoPresupuesto'
-      , headerName: 'Codigo', width: 130
-
+      flex: 0.02,
+      minWidth: 80,
+      sortable: false,
+      field: 'actions',
+      headerName: 'Actions',
+      renderCell: ({ row }: CellType) => (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Tooltip title='Reporte Resumen'>
+            <IconButton size='small' onClick={() => reportView(row)}>
+              <Icon icon='mdi:eye-outline' fontSize={20} />
+            </IconButton>
+          </Tooltip>
+        </Box>
+      )
     },
     {
-
+      field: 'codigoPresupuesto',
+      headerName: 'Codigo',
+      width: 130
+    },
+    {
       field: 'denominacion',
       width: 430
-
     },
 
     {
-      headerName:'',
+      headerName: '',
       field: 'ano',
       width: 100
     },
 
-
-
     {
-     with:100,
+      with: 100,
       headerName: 'Desde',
       field: 'fechaDesde',
       renderCell: (params: GridRenderCellParams) => (
         <Typography variant='body2' sx={{ color: 'text.primary' }}>
-          {dayjs(params.row.fechaDesde).format('DD/MM/YYYY') }
+          {dayjs(params.row.fechaDesde).format('DD/MM/YYYY')}
         </Typography>
       )
     },
     {
-      with:100,
+      with: 100,
       headerName: 'Hasta',
       field: 'fechaHasta',
       renderCell: (params: GridRenderCellParams) => (
         <Typography variant='body2' sx={{ color: 'text.primary' }}>
-          {dayjs(params.row.fechaHasta).format('DD/MM/YYYY') }
+          {dayjs(params.row.fechaHasta).format('DD/MM/YYYY')}
         </Typography>
       )
-    },
-
-
-
+    }
   ]
 
-  const handleView=  (row : IPresupuesto)=>{
-
+  const handleView = (row: IPresupuesto) => {
     dispatch(setPresupuesto(row))
 
-     // Operacion Crud 2 = Modificar presupuesto
-    dispatch(setOperacionCrudPresupuesto(2));
+    // Operacion Crud 2 = Modificar presupuesto
+    dispatch(setOperacionCrudPresupuesto(2))
     dispatch(setVerPresupuestoActive(true))
-
-
   }
-  const handleDoubleClick=(row:any)=>{
-
+  const handleClick = (row: any) => {
+    dispatch(setPresupuesto(row))
+  }
+  const handleDoubleClick = (row: any) => {
     handleView(row.row)
-}
-  const handleAdd=  ()=>{
+  }
 
+  const reportView = async (row: IPresupuesto) => {
+    console.log('IPreAsignacionesDetalleGetDto', row)
 
+    setLoading(true)
+
+    const filter = {
+      codigoPresupuesto: row.codigoPresupuesto
+    }
+    const responseAll = await ossmmasofApi.post<any>('/ReportPreResumenSaldo/GeneratePdf', filter)
+    console.log(responseAll)
+
+    dispatch(setReportName(responseAll.data))
+    dispatch(setVerReportViewActive(true))
+    setLoading(false)
+  }
+
+  const handleAdd = () => {
     const defaultValues = {
       codigoPresupuesto: 0,
-      denominacion:'',
-      descripcion:'',
-      ano:0,
-      numeroOrdenanza:'',
-      extra1:'',
-      extra2:'',
-      extra3:'',
-      fechaDesde:fechaActual,
-      fechaDesdeString:defaultDateString,
-      fechaDesdeObj:defaultDate,
-      fechaHasta:fechaActual,
-      fechaHastaString:defaultDateString,
-      fechaHastaObj :defaultDate,
-      fechaOrdenanza :fechaActual,
-      fechaOrdenanzaString :defaultDateString,
-      fechaOrdenanzaObj :defaultDate,
-      fechaAprobacion :fechaActual,
-      fechaAprobacionString :defaultDateString,
-      fechaAprobacionObj :defaultDate
-
+      denominacion: '',
+      descripcion: '',
+      ano: 0,
+      numeroOrdenanza: '',
+      extra1: '',
+      extra2: '',
+      extra3: '',
+      fechaDesde: fechaActual,
+      fechaDesdeString: defaultDateString,
+      fechaDesdeObj: defaultDate,
+      fechaHasta: fechaActual,
+      fechaHastaString: defaultDateString,
+      fechaHastaObj: defaultDate,
+      fechaOrdenanza: fechaActual,
+      fechaOrdenanzaString: defaultDateString,
+      fechaOrdenanzaObj: defaultDate,
+      fechaAprobacion: fechaActual,
+      fechaAprobacionString: defaultDateString,
+      fechaAprobacionObj: defaultDate
     }
 
-
-    dispatch(setPresupuesto(defaultValues));
+    dispatch(setPresupuesto(defaultValues))
 
     // Operacion Crud 1 = Crear presupuesto
-    dispatch(setOperacionCrudPresupuesto(1));
+    dispatch(setOperacionCrudPresupuesto(1))
     dispatch(setVerPresupuestoActive(true))
-
-
   }
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   // {presupuestos,isLoading,isError }= usePresupuesto('/PrePresupuesto/GetAll');
-  const {verPresupuestoActive=false} = useSelector((state: RootState) => state.presupuesto)
-  const [loading, setLoading] = useState(false);
+  const { verPresupuestoActive = false } = useSelector((state: RootState) => state.presupuesto)
+  const [loading, setLoading] = useState(false)
 
-  const [presupuestos, setPresupuestos] = useState([]);
+  const [presupuestos, setPresupuestos] = useState([])
 
   useEffect(() => {
-
     const getPresupuestos = async () => {
-      setLoading(true);
+      setLoading(true)
 
-      const responseAll= await ossmmasofApi.get<any>('/PrePresupuesto/GetList');
-      const data = responseAll.data;
+      const responseAll = await ossmmasofApi.get<any>('/PrePresupuesto/GetList')
+      const data = responseAll.data
 
-      setPresupuestos(data);
+      setPresupuestos(data)
 
-      setLoading(false);
-    };
-     getPresupuestos();
-
-
-
-  }, [verPresupuestoActive]);
-
-
-
+      setLoading(false)
+    }
+    getPresupuestos()
+  }, [verPresupuestoActive])
 
   return (
     <Grid item xs={12}>
-       <Card>
-      <CardHeader title='Maestro de Presupuesto' />
+      <Card>
+        <CardHeader title='Maestro de Presupuesto' />
 
-      <CardActions>
+        <CardActions>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Tooltip title='Agregar'>
+              <IconButton color='primary' size='small' onClick={() => handleAdd()}>
+                <Icon icon='ci:add-row' fontSize={20} />
+              </IconButton>
+            </Tooltip>
+          </Box>
+        </CardActions>
 
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <Tooltip title='Agregar'>
-            <IconButton  color='primary' size='small' onClick={() => handleAdd()}>
-            <Icon icon='ci:add-row' fontSize={20} />
-            </IconButton>
-          </Tooltip>
-
-        </Box>
-      </CardActions>
-
-
-      {
-        loading
-        ?   <Spinner sx={{ height: '100%' }} />
-        :
-         <Box sx={{ height: 500 }}>
-          <DataGrid
-           getRowId={(row) => row.codigoPresupuesto}
-           columns={columns}
-           rows={presupuestos}
-           onRowDoubleClick={(row) => handleDoubleClick(row)}
-           />
-        </Box>
-
-      }
-      <DatePickerWrapper>
-        <DialogPrePresupuestoInfo  popperPlacement={popperPlacement} />
-      </DatePickerWrapper>
-
-    </Card>
+        {loading ? (
+          <Spinner sx={{ height: '100%' }} />
+        ) : (
+          <Box sx={{ height: 500 }}>
+            <DataGrid
+              getRowId={row => row.codigoPresupuesto}
+              columns={columns}
+              rows={presupuestos}
+              onRowClick={row => handleClick(row)}
+              onRowDoubleClick={row => handleDoubleClick(row)}
+            />
+          </Box>
+        )}
+        <DatePickerWrapper>
+          <DialogReportInfo></DialogReportInfo>
+          <DialogPrePresupuestoInfo popperPlacement={popperPlacement} />
+        </DatePickerWrapper>
+      </Card>
     </Grid>
-
-
   )
 }
 
