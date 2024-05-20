23cfcc517d849dadc15b837ad8678e4cd3b9fde3
reveronfranklin <reveronfranklin@gmail.com>
2024-05-19 20:22:32 -0400

    Se muestra presupuesto seleccionado por defecto en FilterOnlyPresupuesto.

--- src/views/forms/form-elements/presupuesto/FilterOnlyPresupuesto.tsx
+++ src/views/forms/form-elements/presupuesto/FilterOnlyPresupuesto.tsx
@@ -1,5 +1,5 @@
 // ** React Imports
-import { useEffect} from 'react'
+import { useEffect } from 'react'
 
 // ** MUI Imports
 import Box from '@mui/material/Box'
@@ -19,110 +19,69 @@ import { RootState } from 'src/store'
 
 import { fetchDataListPresupuestoDto } from 'src/store/apps/presupuesto/thunks'
 
-
-
-import { setListpresupuestoDtoSeleccionado} from 'src/store/apps/presupuesto'
+import { setListpresupuestoDtoSeleccionado } from 'src/store/apps/presupuesto'
 import { IListPresupuestoDto } from 'src/interfaces/Presupuesto/i-list-presupuesto-dto'
-
-
 
 //const FilterPresupuesto = ({ popperPlacement }: { popperPlacement: ReactDatePickerProps['popperPlacement'] }) => {
 
 const FilterOnlyPresupuesto = () => {
+  const dispatch = useDispatch()
 
-
-  const dispatch = useDispatch();
-
-  const {listpresupuestoDto} = useSelector((state: RootState) => state.presupuesto)
-
-
-
-
+  const { listpresupuestoDto, listpresupuestoDtoSeleccionado } = useSelector((state: RootState) => state.presupuesto)
 
   // ** States
 
   //const [conceptosPorTipoNomina, setConceptosPorTipoNomina] = useState<IListConceptosDto[]>(conceptos)
 
+  const handlePresupuestos = async (e: any, value: any) => {
+    if (value) {
+      dispatch(setListpresupuestoDtoSeleccionado(value))
+    } else {
+      const presupuesto: IListPresupuestoDto = {
+        ano: 0,
+        codigoPresupuesto: 0,
+        descripcion: '',
+        preFinanciadoDto: [],
+        presupuestoEnEjecucion: false
+      }
 
-  const handlePresupuestos= async (e: any,value:any)=>{
-
-
-    if(value){
-
-
-
-      dispatch(setListpresupuestoDtoSeleccionado(value));
-
-
-
-    }else{
-
-      const presupuesto:IListPresupuestoDto ={
-        ano:0,
-        codigoPresupuesto:0,
-        descripcion:'',
-        preFinanciadoDto:[],
-        presupuestoEnEjecucion:false
-
-      };
-
-      dispatch(setListpresupuestoDtoSeleccionado(presupuesto));
+      dispatch(setListpresupuestoDtoSeleccionado(presupuesto))
     }
-
-
   }
 
-
-
-
   useEffect(() => {
-
     const getData = async () => {
-
-      await fetchDataListPresupuestoDto(dispatch);
-
-
-    };
-    getData();
-
-
-
-  }, [dispatch]);
+      await fetchDataListPresupuestoDto(dispatch)
+    }
+    getData()
+  }, [dispatch])
 
   return (
     <Grid item xs={12}>
-    <Card>
-      <CardHeader title='Filtrar Presupuesto' />
-      <CardContent>
-        <Grid container spacing={6}>
-          <Grid item xs={12} >
-          <Box sx={{ display: 'flex', flexWrap: 'wrap' }} className='demo-space-x'>
-
-
-              <div>
-                <Autocomplete
+      <Card>
+        <CardHeader title='Filtrar Presupuesto' />
+        <CardContent>
+          <Grid container spacing={6}>
+            <Grid item xs={12}>
+              <Box sx={{ display: 'flex', flexWrap: 'wrap' }} className='demo-space-x'>
+                <div>
+                  <Autocomplete
                     sx={{ width: 350 }}
                     options={listpresupuestoDto}
+                    defaultValue={listpresupuestoDtoSeleccionado}
                     id='autocomplete-MaestroPresupuesto'
-                    isOptionEqualToValue={(option, value) => option.codigoPresupuesto=== value.codigoPresupuesto}
+                    isOptionEqualToValue={(option, value) => option.codigoPresupuesto === value.codigoPresupuesto}
                     getOptionLabel={option => option.codigoPresupuesto + '-' + option.descripcion}
                     onChange={handlePresupuestos}
                     renderInput={params => <TextField {...params} label='Presupuesto' />}
                   />
-              </div>
-
-
-
-
-
-          </Box>
-        </Grid>
-
-        </Grid>
-      </CardContent>
-    </Card>
-  </Grid>
-
+                </div>
+              </Box>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Grid>
   )
 }
 
