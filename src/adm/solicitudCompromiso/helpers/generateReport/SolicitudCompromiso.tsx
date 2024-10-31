64fe285d1eb4611687e1a4e41e3c8987d93d2ff5
jesus alberto garcia hernandez <jesusalberto278@gmail.com>
2024-10-30 20:26:33 -0400

    refactor: rename report handling functions and improve error handling

--- src/adm/solicitudCompromiso/helpers/generateReport/SolicitudCompromiso.tsx
+++ src/adm/solicitudCompromiso/helpers/generateReport/SolicitudCompromiso.tsx
@@ -1,24 +1,23 @@
 const HandleReport = async (props: {
-    filter: any
-    geReportUrl: (filter: any) => Promise<any>
+    filter: any,
+    geReportUrl: (filter: any) => Promise<any>,
     downLoadReport: (url: string) => Promise<any>
-}) => {
+}): Promise<any> => {
     const { filter, geReportUrl, downLoadReport } = props
 
     if (!geReportUrl || !filter) {
         return
     }
 
-    const responseGetReportUrl = await geReportUrl(filter)
+    try {
+        const responseGetReportUrl = await geReportUrl(filter)
 
-    if (responseGetReportUrl.isValid) {
-        const url = responseGetReportUrl?.data
-
-        try {
-            await downLoadReport(url)
-        } catch (e: any) {
-            console.error(e)
+        if (responseGetReportUrl.isValid && responseGetReportUrl.data) {
+            await downLoadReport(responseGetReportUrl.data)
         }
+
+    } catch (e: any) {
+        console.error(e)
     }
 }
 
