96254bd128dcf772f8295c0248d050222dc8f428
jesus alberto garcia hernandez <jesusalberto278@gmail.com>
2025-03-20 13:03:43 -0400

    fix

--- src/adm/ordenesPago/enums/UrlServices.enum.ts
+++ src/adm/ordenesPago/enums/UrlServices.enum.ts
@@ -1,7 +1,7 @@
 export enum UrlServices {
     GETORDENESPAGOBYPRESUPUESTO = '/AdmOrdenPago/GetByPresupuesto',
 
-    GETCOMPROMISOSBYPRESUPUESTO = '/PreCompromisos/GetByPresupuesto',
+    GETCOMPROMISOSBYPRESUPUESTO          = '/PreCompromisos/GetByPresupuesto',
     GETCOMPROMISOSPENDIENTEBYPRESUPUESTO = '/PreCompromisos/GetCompromisosPendientesByPresupuesto',
 
     GETPUCORDENPAGO             = '/AdmPucOrdenPago/GetByOrdenPago',
@@ -29,14 +29,20 @@ export enum UrlServices {
     UPDATEDOCUMENTOSOP          = '/AdmDocumentosOp/Update',
     DELETEDOCUMENTOSOP          = '/AdmDocumentosOp/Delete',
 
+    GETIMPUESTOSDOCBYORDENPAGO  = '/AdmImpuestosDocumentosOp/GetByDocumento',
+    CREATEIMPUESTODOCUMENTO     = '/AdmImpuestosDocumentosOp/Create',
+    DELETEIMPUESTODOCUMENTO     = '/AdmImpuestosDocumentosOp/Delete',
+    UPDATEIMPUESTODOCUMENTO     = '/AdmImpuestosDocumentosOp/Update',
+
     GETBENEFICIARIO             = '/AdmBeneficiariosOp/GetByOrdenPago',
     CREATEBENEFICIARIO          = '/AdmBeneficiariosOp/Update',
     UPDATEBENEFICIARIO          = '/AdmBeneficiariosOp/Create',
     DELETEBENEFICIARIO          = '/AdmBeneficiariosOp/Delete',
     UPDATEBENEFICIARIOMONTO     = '/AdmBeneficiariosOp/UpdateMonto',
 
-    GETREPORTBYORDENPAGO        = 'payment-orders/pdf/report',
-    GETREPORTBYRETENCIONES      = 'income-tax-withholding-voucher/pdf/report',
-    GETREPORTBYCOMPROBANTE      = 'vat-withholding-voucher/pdf/report',
-    TIMBREFISCAL                = 'tax-stamp-voucher/pdf/report'
+    //Urls Reports
+    GETREPORTBYORDENPAGO        = '/api-v1.0/payment-orders/pdf/report',
+    GETREPORTBYRETENCIONES      = '/api-v1.0/income-tax-withholding-voucher/pdf/report',
+    GETREPORTBYCOMPROBANTE      = '/api-v1.0/vat-withholding-voucher/pdf/report',
+    TIMBREFISCAL                = '/api-v1.0/tax-stamp-voucher/pdf/report'
 }
