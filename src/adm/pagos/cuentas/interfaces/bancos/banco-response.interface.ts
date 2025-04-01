d60e469bdd304e95e8ef6566f3dfd535f5d19e50
Abraham Rivas <abraham15rivas@gmail.com>
2025-04-01 16:09:01 -0400

    first changes OSS-135

---
+++ src/adm/pagos/cuentas/interfaces/bancos/banco-response.interface.ts
@@ -0,0 +1,6 @@
+export interface IBancoResponse {
+    codigoBanco: number
+    nombre: string
+    codigoInterbancario: string
+    searchText: string
+}
---
+++ src/adm/pagos/cuentas/services/useServices.ts
@@ -0,0 +1,93 @@
+import { useCallback, useState } from "react"
+import { ossmmasofApi } from 'src/MyApis/ossmmasofApi'
+import { UrlServices } from '../enums/urlServices.enum'
+//import { IResponse, SisBancoResponseDto, SisBancoFilterDto, SisBancoCreateDto, SisBancoUpdateDto, SisBancoDeleteDto } from '../interfaces'
+import { IResponse, IBancoFilter, IBancoResponse } from '../interfaces'
+
+const useServices = () => {
+    const [error, setError] = useState<string>('')
+    const [message, setMessage] = useState<string>('')
+    const [loading, setLoading] = useState<boolean>(false)
+
+    const getMaestroBanco = useCallback(async (filters: IBancoFilter): Promise<any> => {
+        try {
+            setLoading(true)
+            const responseGetMaestroBanco = await ossmmasofApi.post<IResponse<IBancoResponse>>(UrlServices.GETMAESTROBANCO , filters)
+
+            if (responseGetMaestroBanco.data.isValid) {
+                return responseGetMaestroBanco.data
+            }
+
+            setMessage(responseGetMaestroBanco.data.message)
+        } catch (e: any) {
+            setError(e.message)
+            console.error(e)
+        } finally {
+            setLoading(false)
+        }
+    }, [])
+
+    /* const createMaestroBanco = useCallback(async (payload: SisBancoCreateDto): Promise<any> => {
+        try {
+            setLoading(true)
+            const responseCreateMaestroBanco = await ossmmasofApi.post<IResponse<SisBancoResponseDto>>(UrlServices.CREATEMAESTROBANCO, payload)
+
+            if (responseCreateMaestroBanco.data.isValid) {
+                return responseCreateMaestroBanco.data
+            }
+
+            setMessage(responseCreateMaestroBanco.data.message)
+        } catch (e: any) {
+            setError(e.message)
+            console.error(e)
+        } finally {
+            setLoading(false)
+        }
+    }, [])
+
+    const updateMaestroBanco = useCallback(async (payload: SisBancoUpdateDto): Promise<any> => {
+        try {
+            setLoading(true)
+            const responseUpdateMaestroBanco = await ossmmasofApi.post<IResponse<SisBancoResponseDto>>(UrlServices.UPDATEMAESTROBANCO, payload)
+
+            if (responseUpdateMaestroBanco.data.isValid) {
+                return responseUpdateMaestroBanco.data
+            }
+
+            setMessage(responseUpdateMaestroBanco.data.message)
+        } catch (e: any) {
+            setError(e.message)
+            console.error(e)
+        } finally {
+            setLoading(false)
+        }
+    }, [])
+
+    const deleteMaestroBanco = useCallback(async (payload: SisBancoDeleteDto): Promise<any> => {
+        try {
+            setLoading(true)
+            const responseDeleteMaestroBanco = await ossmmasofApi.post<IResponse<SisBancoDeleteDto>>(UrlServices.DELETEMAESTROBANCO, payload)
+
+            if (responseDeleteMaestroBanco.data.isValid) {
+                return responseDeleteMaestroBanco.data
+            }
+
+            setMessage(responseDeleteMaestroBanco.data.message)
+        } catch (e: any) {
+            setError(e.message)
+            console.error(e)
+        } finally {
+            setLoading(false)
+        }
+    }, [])
+ */
+    return {
+        error,
+        message,
+        loading,
+        setMessage,
+        getMaestroBanco
+    }
+}
+
+export default useServices
