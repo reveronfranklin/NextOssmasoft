979f955c5b104b82c06a879adbfce250d3cf0a0c
Abraham Rivas <abraham15rivas@gmail.com>
2025-06-12 14:44:15 -0400

    added create whitout Payment Order

--- src/adm/pagos/lotes/hooks/usePause.ts
+++ src/adm/pagos/lotes/hooks/usePause.ts
@@ -1,20 +1,20 @@
 import { useState } from 'react'
 
 const usePause = () => {
-  const [isPaused, setIsPaused] = useState(false)
+    const [isPaused, setIsPaused] = useState(false)
 
-  const pause = async (duration: number): Promise<void> => {
-    setIsPaused(true)
-    
-return new Promise((resolve) => {
-      setTimeout(() => {
-        setIsPaused(false)
-        resolve()
-      }, duration)
-    })
-  }
+    const pause = async (duration: number): Promise<void> => {
+        setIsPaused(true)
 
-  return { isPaused, pause }
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                setIsPaused(false)
+                resolve()
+            }, duration)
+        })
+    }
+
+    return { isPaused, pause }
 }
 
 export default usePause
