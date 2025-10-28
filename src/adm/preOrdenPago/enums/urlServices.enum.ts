60f9bb81b8b5bd747a13239517eb12bbf041e38d
Abraham Rivas <abraham15rivas@gmail.com>
2025-10-28 14:57:58 -0400

    new changes

--- src/adm/preOrdenPago/enums/urlServices.enum.ts
+++ src/adm/preOrdenPago/enums/urlServices.enum.ts
@@ -1,4 +1,4 @@
 export enum UrlServices {
     GET_PRE_ORDEN_PAGO      = '/AdmPreOrdenPago/GetAll',
-    CREATE_PRE_ORDEN_PAGO   = '/AdmPreOrdenPago/CreateLote'
+    CREATE_PRE_ORDEN_PAGO   = '/invoices/load',
 }
