8461783f10436725aa2317ec9d5a81d351e2d5bb
reveronfranklin <reveronfranklin@gmail.com>
2026-06-15 10:31:14 -0400

    Refactore menu from json menu

--- src/layouts/components/acl/CanViewNavGroup.tsx
+++ src/layouts/components/acl/CanViewNavGroup.tsx
@@ -1,11 +1,8 @@
 // ** React Imports
-import { ReactNode, useContext } from 'react'
-
-// ** Component Imports
-import { AbilityContext } from 'src/layouts/components/acl/Can'
+import { ReactNode } from 'react'
 
 // ** Types
-import { NavGroup, NavLink } from 'src/@core/layouts/types'
+import { NavGroup } from 'src/@core/layouts/types'
 
 interface Props {
   navGroup?: NavGroup
@@ -14,34 +11,9 @@ interface Props {
 
 const CanViewNavGroup = (props: Props) => {
   // ** Props
-  const { children, navGroup } = props
+  const { children } = props
 
-  // ** Hook
-  const ability = useContext(AbilityContext)
-
-  const canViewMenuItem = (item: NavGroup | NavLink): boolean => {
-    if ('children' in item && item.children) {
-      return canViewMenuGroup(item)
-    }
-
-    if (!(item.action && item.subject)) {
-      return true
-    }
-
-    return ability ? ability.can(item.action, item.subject) : false
-  }
-
-  const canViewMenuGroup = (item: NavGroup): boolean => {
-    const hasAnyVisibleChild = item.children?.some(child => canViewMenuItem(child)) ?? false
-
-    if (!(item.action && item.subject)) {
-      return hasAnyVisibleChild
-    }
-
-    return ability && ability.can(item.action, item.subject) && hasAnyVisibleChild
-  }
-
-  return navGroup && canViewMenuGroup(navGroup) ? <>{children}</> : null
+  return <>{children}</>
 }
 
 export default CanViewNavGroup
